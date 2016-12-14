9f033fefbcc712b45309ce3a747ef2b8c61e8abd
YuuNi <halhal.halgerda@gmail.com>
2016-12-14 16:33:01 +0900

    環境周り整備

--- app/src/main/java/jp/co/rjc/kokoshake/ui/activity/SettingsActivity.java
+++ app/src/main/java/jp/co/rjc/kokoshake/ui/activity/SettingsActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.example.t_nakahata.kokoshake_android.R;
+import jp.co.rjc.kokoshake.R;
 
 /**
  * 設定画面のActivity

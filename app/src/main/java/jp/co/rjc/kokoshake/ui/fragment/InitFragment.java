1aa5a5e645cf3e9d515ae807b5e6b873243fdc3c
RJC\s_kabamoto <rrrtestjc@gmail.com>
2017-01-24 17:02:00 +0900

    修正＆追加分をコミット

--- app/src/main/java/jp/co/rjc/kokoshake/ui/fragment/InitFragment.java
+++ app/src/main/java/jp/co/rjc/kokoshake/ui/fragment/InitFragment.java
@@ -1,19 +1,17 @@
 package jp.co.rjc.kokoshake.ui.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import jp.co.rjc.kokoshake.R;
-import jp.co.rjc.kokoshake.ui.activity.SettingsActivity;
 
+/**
+ * 初期画面用フラグメント.
+ */
 public class InitFragment extends Fragment {
-
-    private ImageView mSettingBtn;
 
     public InitFragment() {
     }
@@ -26,20 +24,9 @@ public class InitFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-
-        mSettingBtn = (ImageView) view.findViewById(R.id.setting_button);
-
-        mSettingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SettingsActivity.class);
-                startActivity(intent);
-                //getActivity().finish();
-            }
-        });
+        View view = inflater.inflate(R.layout.fragment_init, container, false);
 
         return view;
-}
+    }
 
 }
--- app/src/main/java/jp/co/rjc/kokoshake/ui/fragment/MainFragment.java
+++ app/src/main/java/jp/co/rjc/kokoshake/ui/fragment/MainFragment.java
@@ -1,19 +1,17 @@
 package jp.co.rjc.kokoshake.ui.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import jp.co.rjc.kokoshake.R;
-import jp.co.rjc.kokoshake.ui.activity.SettingsActivity;
 
+/**
+ * 初回起動チュートリアル用フラグメント.
+ */
 public class MainFragment extends Fragment {
-
-    private ImageView mSettingBtn;
 
     public MainFragment() {
     }
@@ -28,18 +26,7 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-        mSettingBtn = (ImageView) view.findViewById(R.id.setting_button);
-
-        mSettingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SettingsActivity.class);
-                startActivity(intent);
-                //getActivity().finish();
-            }
-        });
-
         return view;
-}
+    }
 
 }
